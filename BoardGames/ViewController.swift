e2e5ee02583b5e4580fec7eaa620097fb4f68830
IlyaMarkelov <marckellow@icloud.com>
2021-11-12 18:42:31 +0300

    Initial Commit

---
+++ BoardGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoardGames
+//
+//  Created by Илья Маркелов on 12.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
