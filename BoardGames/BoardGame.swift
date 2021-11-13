64bc89d3b59b713fdd04e3075b97a0481b3246b9
IlyaMarkelov <marckellow@icloud.com>
2021-11-13 22:21:48 +0300

    work with table view

---
+++ BoardGames/BoardGame.swift
@@ -0,0 +1,30 @@
+//
+//  Model.swift
+//  BoardGames
+//
+//  Created by Илья Маркелов on 13.11.2021.
+//
+
+struct BoardGame {
+    let name: String
+    let description: String
+    let price: String
+}
+
+extension BoardGame {
+    static func getBoardGamesList() -> [BoardGame] {
+        var boardGames: [BoardGame] = []
+        
+        let data = DataManager.shared
+        
+        for index in 0..<data.name.count {
+            let boardGame = BoardGame(
+                name: data.name[index],
+                description: data.description[index],
+                price: data.price[index]
+            )
+            boardGames.append(boardGame)
+        }
+        return boardGames
+    }
+}
---
+++ BoardGames/BoardGamesListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BoardGamesListViewController.swift
+//  BoardGames
+//
+//  Created by Илья Маркелов on 13.11.2021.
+//
+
+import UIKit
+
+class BoardGamesListViewController: UITableViewController {
+    
+    var boardGamesList = BoardGame.getBoardGamesList()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 80
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        boardGamesList.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let boardGame = boardGamesList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = boardGame.name
+        content.secondaryText = boardGame.price
+        
+        let itemSize = CGSize.init(width: 70, height: 70)
+        UIGraphicsBeginImageContextWithOptions(itemSize, false, UIScreen.main.scale)
+        let imageRect = CGRect.init(origin: CGPoint.zero, size: itemSize)
+        content.image = UIImage(named: boardGame.name)
+        content.image?.draw(in: imageRect)
+        content.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        content.imageProperties.cornerRadius = tableView.rowHeight / 2
+        cell.contentConfiguration = content
+        return cell
+    }
+}
